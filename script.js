f2ef4fb4e4ddadb03cce3bc885f3e29550dc69a1
Rojus Stoncius <90309780+Rojus24@users.noreply.github.com>
2021-10-15 10:23:48 +0000

    alles gekopierd van u

--- script.js
+++ script.js
@@ -1,60 +1,26 @@
-function setup() {
-	createCanvas(500, 400);
-}
+var x, y, vx, vy;
 
-var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
+function setup() {
+	createCanvas(400, 300);
+
+  x = 0;
+  y = 150;
+  vx = 6;
+  vy = 6;
+}
 
 function draw() {
 	background(225);
-	
-	fill(0);
-	text("Use the arrow keys (or WASD) to move the square around", 25, 25);
-	
-	fill(0, 255, 0);
-	rect(xpos, ypos, 50, 50);
-	
-	if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
-	if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
-}
+  
+  ellipse(x,y,50,50);
+  x = x + vx;
+  y = y + vy;
 
-function keyPressed() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = -2;
-			break;
-		case 39:
-		case 68:
-			xspeed = 2;
-			break;
-		case 38:
-		case 87:
-			yspeed = -2;
-			break;
-		case 40:
-		case 83:
-			yspeed = 2;
-			break;
-	}
-}
+  if(x <= 0 || x >= width){
+    vx = vx * -1;
+  }
 
-function keyReleased() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = 0;
-			break;
-		case 39:
-		case 68:
-			xspeed = 0;
-			break;
-		case 38:
-		case 87:
-			yspeed = 0;
-			break;
-		case 40:
-		case 83:
-			yspeed = 0;
-			break;
-	}
+   if(y <= 0 || y >= height){
+    vy = vy * -1;
+  }
 }
