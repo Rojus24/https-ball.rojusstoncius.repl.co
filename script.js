87e4f8ab9ac1057db59d27d247a71accc899b25f
Rojus Stoncius <90309780+Rojus24@users.noreply.github.com>
2021-10-22 17:35:06 +0000

    meer balletjes toegevoed en kleur gegeven

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ class Ball {
     this.height = h;
     this.vx = vx;
     this.vy = vy;
+    this.color = color
   }
 
   draw() {
@@ -13,28 +14,37 @@ class Ball {
     this.x = this.x + this.vx;
     this.y = this.y + this.vy;
 
-    if (this.x < 25 || this.x >= 475) {
+    if(this.x < 25 || this.x >= 475){
       this.vx = this.vx * -1;
     }
 
-    if (this.y < 25 || this.y >= 375) {
+    if(this.y < 25 || this.y >= 375){
       this.vy = this.vy * -1;
     }
   }
 }
 
-var ball1;
+var ball1, ball2, ball3;
 
 function setup() {
   createCanvas(500, 400);
-
-  ball1 = new Ball(30, 200, 50, 50, 5, 5);
+  
+  ball1 = new Ball(30, 200, 50, 50, 5, 5,);
+  ball2 = new Ball(200, 20, 80, 80, 15, 8);
+  ball3 = new Ball(40, 80, 70, 70, 7, 10);
+ 
 }
 
 
-  function draw() {
-    background(225);
+function draw() {
+  background(225);
 
-    ball1.draw();
-  }
+  fill ('turquoise')
+  ball1.draw();
+  fill ('orange')
+  ball2.draw();
+  fill ('green')
+  ball3.draw();
 
+ 
+}
