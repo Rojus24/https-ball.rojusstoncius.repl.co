74f95c2f0119b976986b966cc2f23fbb01fc0ac8
Rojus Stoncius <90309780+Rojus24@users.noreply.github.com>
2021-10-22 15:56:33 +0000

    draw functie toegevoegd

--- script.js
+++ script.js
@@ -8,31 +8,23 @@ class Ball {
     this.vy = vy;
   }
 
-  
+  draw() {
+    ellipse(this.x, this.y, 50, 50);
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+    if (this.x < 25 || this.x >= 475) {
+      this.vx = this.vx * -1;
+    }
+
+    if (this.y < 25 || this.y >= 375) {
+      this.vy = this.vy * -1;
+    }
+  }
+}
+
+
 function draw() {
   background(225);
 
-  ellipse(x, y, 50, 50);
-  x = x + vx;
-  y = y + vy;
-
-  if (x <= 0 || x >= width) {
-    vx = vx * -1;
-  }
-
-  if (y <= 0 || y >= height) {
-    vy = vy * -1;
-  }
-
-  ellipse(x2, y2, 50, 50);
-  x2 = x2 + vx2;
-  y2 = y2 + vy2;
-
-  if (x2 <= 0 || x2 >= width) {
-    vx2 = vx2 * -1;
-  }
-
-  if (y2 <= 0 || y2 >= height) {
-    vy2 = vy2 * -1;
-  }
-}
+ 
